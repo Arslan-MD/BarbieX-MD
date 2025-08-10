ff0ecddac629f42c581a38534471fa39c636423b
ArslanMD Official <numberkliye@gmail.com>
2025-08-10 09:50:09 +0500

    Update menu-old.js

--- plugins/menu-old.js
+++ plugins/menu-old.js
@@ -46,7 +46,7 @@ async (conn, mek, m, { from, sender, pushname, reply }) => {
         await conn.sendMessage(
             from,
             {
-                image: { url: config.MENU_IMAGE_URL },
+                video: { url: config.MENU_VIDEO_URL },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
@@ -54,7 +54,7 @@ async (conn, mek, m, { from, sender, pushname, reply }) => {
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363348739987203@newsletter',
-                        newsletterName: '𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃',
+                        newsletterName: 'BarbieX-MD',
                         serverMessageId: 143
                     }
                 }
